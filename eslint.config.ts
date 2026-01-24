e20ba24457ed9d956bd6986a95a6b1291527d561
Justin <justin.blair.rankin@gmail.com>
2026-01-24 15:43:49 +0100

    test: comment out markdown layer

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'eslint/config';
 
 import { base } from './src/layers/base.layer';
 import { imports } from './src/layers/imports.layer';
-import { markdown } from './src/layers/markdown.layer';
+// import { markdown } from './src/layers/markdown.layer';
 import { node } from './src/layers/node.layer';
 import { typescript } from './src/layers/typescript.layer';
 import { typescriptTyped } from './src/layers/typescript-typed.layer';
@@ -11,7 +11,7 @@ export default defineConfig(
   { ignores: ['templates/**', 'dist/**', 'node_modules/**'] },
   ...base(),
   ...imports(),
-  ...markdown(),
+  // ...markdown(),
   ...node(),
   ...typescript(),
   ...typescriptTyped({
