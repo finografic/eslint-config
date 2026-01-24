043ecd27a954fe9ea8764869abad1780879bfaf9
Justin <justin.blair.rankin@gmail.com>
2026-01-24 15:41:24 +0100

    deps: deps fix
    deps: deps fix

--- src/layers/markdown.layer.ts
+++ src/layers/markdown.layer.ts
@@ -29,7 +29,7 @@ export function markdown(): ESLintConfig[] {
 
       plugins: {
         markdownlint: markdownlintPlugin,
-        stylistic: stylisticPlugin,
+        '@stylistic': stylisticPlugin,
       },
 
       rules: {
